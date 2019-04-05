ea3a541702f454c15ed222d741ad96ca34b4906d
Neildor Davis <superman4kryptonite@hotmail.com>
2019-04-05 22:03:21 +0100

    git add *

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+</head>
+<body>
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fuga voluptatibus, dolorum dolor voluptates, voluptatum quas voluptate officiis ad, nesciunt totam neque voluptas aspernatur. Aspernatur cupiditate cum quasi dicta explicabo.</p>
+</body>
+</html>
